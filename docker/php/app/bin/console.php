04800ea843919414573ffa9278b3d6d3b22ff627
Hemery, Thomas <thomas.hemery@lexisnexisrisk.com>
2023-01-05 11:30:37 +0000

    Working test

--- docker/php/app/bin/console.php
+++ docker/php/app/bin/console.php
@@ -1,2 +1,28 @@
 <?php
 echo "Cassandra test\n";
+
+sleep(10);
+
+while (true) {
+    try {
+        $cluster = Cassandra::cluster()
+            ->withContactPoints('cassandra')
+            ->build();
+        $keyspace = 'system';
+        $session = $cluster->connect($keyspace);
+        $statement = new Cassandra\SimpleStatement(
+            'describe tables'
+        );
+        $future = $session->executeAsync($statement, []);
+        $result = $future->get(30);
+
+        foreach ($result as $row) {
+            print_r($row);
+        }
+        break;
+    } catch (Exception $e) {
+        echo("Failed to use cassandra with error {$e->getMessage()}\n");
+        sleep(10);
+        echo("Retrying\n");
+    }
+}
